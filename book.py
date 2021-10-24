b04ff668640a81d182dfd2911e672e14dd45509c
Brandon Jackson <usbrandon@gmail.com>
2021-10-24 13:29:35 -0500

    Load regions.xlsx and output the A1 cell.

--- book.py
+++ book.py
@@ -11,3 +11,11 @@ ws2 = wb.create_sheet('Another', 0)
 ws.title = 'MySheet'
 
 print(wb.sheetnames)
+
+wb2 = load_workbook('regions.xlsx')
+
+new_sheet = wb2.create_sheet('NewSheet')
+active_sheet = wb2.active
+
+cell = active_sheet['A1']
+print(cell.value)
